159bd6cfe6875faa576c978db3dc73a442b7b9cb
rsjch1-art <rsjch1@gmail.com>
2026-03-08 00:35:10 -0600

    fiumba no se que hago

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import os
 import psycopg2
+import urllib.parse as up
 
-# Lee la variable de entorno que Railway inyecta
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-# Conéctate usando esa URL
-conn = psycopg2.connect(DATABASE_URL)
+# Asegúrate que empiece con postgres://
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgres://", 1)
+
+up.uses_netloc.append("postgres")
+url = up.urlparse(DATABASE_URL)
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 cur = conn.cursor()
