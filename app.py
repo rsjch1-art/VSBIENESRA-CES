6ef52a93649dd21688ed4bbca851596b6cf75278
rsjch1-art <rsjch1@gmail.com>
2026-03-08 00:33:17 -0600

    commit deploy

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-import os, psycopg2
+import os
+import psycopg2
+
+# Lee la variable de entorno que Railway inyecta
 DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# Conéctate usando esa URL
 conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
